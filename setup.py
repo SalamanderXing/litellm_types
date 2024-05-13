580b0533e7fc5c8c9f53a78f95e0585d160056ae
salamanderxing <giulio.zani@gmail.com>
2024-05-13 14:28:04 +0200

    async streaming + tool calls

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.system("rm -rf build dist")
 
 setup(
     name="litellm_types",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "pydantic",
